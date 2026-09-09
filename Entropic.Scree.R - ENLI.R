90484fe3eb2f8ad783d1994b9718909c8f297f49
Terrence J. Lee-St. John, PhD <tjleestjohn@gmail.com>
2026-09-09 08:56:16 +0400

    Update version label to 1.0.1 beta

--- Entropic.Scree.R - ENLI.R
+++ Entropic.Scree.R - ENLI.R
@@ -1,7 +1,7 @@
 options(max.print = 9999999)
 
 # ==============================================================================
-# ENTROPIC SCREE Function (v1.0.1)
+# ENTROPIC SCREE Function (1.0.1 beta)
 #
 # Author: Terrence J. Lee-St. John
 # Organization: Enli (www.enli.com.au)
@@ -965,7 +965,7 @@ Entropic.Scree <- function(data
   # WAVE 4: METHODOLOGICAL REFERENCE
   # ============================================================================
   cat("===================================================================================\n")
-  cat(" ENTROPIC SCREE (v1.0.1) - METHODOLOGICAL REFERENCE & LICENSE\n")
+  cat(" ENTROPIC SCREE (1.0.1 beta) - METHODOLOGICAL REFERENCE & LICENSE\n")
   cat("===================================================================================\n")
   cat(" -> Framework developed by Terrence J. Lee-St. John (Enli)\n")
   cat(" -> Released under the Apache License 2.0 (Open Source)\n")
